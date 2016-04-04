78fde31325314be1af2fad76a3d276c01b837d4f
Juan Carlos López <j.carloslopezl@hotmail.com>
2016-04-04 01:17:47 -0500

    Add number operators and methods

---
+++ numbers.rb
@@ -0,0 +1,26 @@
+# Numbers!!
+
+puts 92 * 10 # Multiplication
+
+puts 5 + 5   # Addition
+
+puts 50 - 5  # Substraction
+
+puts 8 / 2   # Division
+
+
+# Methods
+
+8.next  	 # => 9
+
+8.pred		 # => 7
+
+8.odd?	 	 # => false
+
+8.even?		 # => true
+
+8.zero?		 # => false
+
+8.class 	 # => FixNum
+
+8.1.class	 # => Float
