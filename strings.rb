b9103531748b9b50780d1a5e2c547504b2f40759
Juan Carlos López <j.carloslopezl@hotmail.com>
2016-04-07 00:57:23 -0500

    Add string methods

---
+++ strings.rb
@@ -0,0 +1,17 @@
+# String Methods
+
+puts "hello World".capitalize
+puts "JCLOPEZDEV@GMAIL.COM".downcase
+puts "RUBY is FUNNY".swapcase
+puts "Juan Carlos".reverse
+
+puts "This is an Erorr".gsub('Erorr', "Error")
+puts "Change all the letters a".gsub("a","@")
+
+puts " This  string have double spaces ".strip
+puts "Ruby Programming Language".include?('Ruby')
+puts "Ruby Programming Language".start_with?('Ruby')
+puts "Not empty string".empty?
+puts "a".next
+
+"String".methods
